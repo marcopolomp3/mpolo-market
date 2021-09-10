a12164bb83140d9360600f6e16f9bbffc7f87de7
Marco Polo <mpolo@cobiscorp.com>
2021-09-10 10:58:39 -0500

    first commit

---
+++ src/main/java/com/mpolo/market/MpoloMarketApplication.java
@@ -0,0 +1,13 @@
+package com.mpolo.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpoloMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpoloMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpolo/market/MpoloMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpolo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpoloMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
